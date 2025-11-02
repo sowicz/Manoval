016cb65794d58c3528df1753aa01141f39a502e9
Kamil_S <sowicz09@gmail.com>
2025-11-02 13:55:43 +0100

    Dodaj pliki projektów.

---
+++ Manoval.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+int main()
+{
+    std::string img_path = "C:\\Users\\sowic\\Desktop\\manom.jpg";
+
+	Mat img = imread(img_path, IMREAD_COLOR);
+
+	// Verify that the image has been loaded
+	if (img.empty()) {
+		std::cout << "Nie mogę wczytać obrazu!" << std::endl;
+		return -1;
+	}
+	// Convert to grayscale
+	Mat gray;
+	cvtColor(img, gray, COLOR_BGR2GRAY);
+	cv::imshow("Skala szarosci", gray);
+
+
+	waitKey(0);
+
+}
